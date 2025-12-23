fc3aa7ec8d12fdcceaf5c9c63b5aaeefb2dcc822
volkonskaiaem.26 <yekaterina.volkonskaya@gmail.ru>
2025-12-23 10:17:51 +0300

    Initial commit

---
+++ src/main/java/com/example/BulsAndCows/BulsAndCowsApplication.java
@@ -0,0 +1,13 @@
+package com.example.BulsAndCows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulsAndCowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulsAndCowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BulsAndCows/BulsAndCowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BulsAndCows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulsAndCowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
